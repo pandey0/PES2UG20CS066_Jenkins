b94725bd6201d23a04cd10f1d3395c4240b5b11c
Arpit pandey <89128460+pandey0@users.noreply.github.com>
2023-02-17 22:14:51 +0530

    Update hello.cpp

--- Dockerfile/hello.cpp
+++ Dockerfile/hello.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "ARPIT PANDEY\n";
+  cout << "ARPIT PANDEY pes\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
